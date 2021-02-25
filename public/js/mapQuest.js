78919d265873e2b400f9d123e886b4abd584f549
Yufei Zhou <yuz051@ucsd.edu>
2021-02-25 10:43:18 -0800

    finished

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'rBDGZobFqHV4EWixTH5QN4GKG8UpGOXf';
+
+    var map = L.mapquest.map('map', {
+        center: [32.87872751824317, -117.2358645447225],
+        layers: L.mapquest.tileLayer('map'),
+        zoom: 12,
+        zoomControl: false
+    });
+
+    L.marker([32.87872751824317, -117.2358645447225]).addTo(map);
 }
